0c7395c65cfb2bb7bacec2f6dfdfc66bfee83e14
kunl60 <127754709+kunl60@users.noreply.github.com>
2026-06-09 16:55:28 +0200

    type change of use_minimal_feedback in ArgumentParser

--- examples/classification/XORDemo.py
+++ examples/classification/XORDemo.py
@@ -71,7 +71,7 @@ def default_args(**kwargs):
     parser.add_argument("--platform", default="CPU", type=str)
     parser.add_argument("--weighted_clauses", default=True, type=bool)
     parser.add_argument("--epochs", default=60, type=int)
-    parser.add_argument("--use_minimal_feedback", default=True, type=int)
+    parser.add_argument("--use_minimal_feedback", default=True, type=bool)
     args = parser.parse_args()
     for key, value in kwargs.items():
         if key in args.__dict__:
